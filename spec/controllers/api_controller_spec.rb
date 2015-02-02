899ca3a4292c4bc5f22f0f415fbe89517d2c20ee
HiroshiOyamada <h5y1m141@gmail.com>
2015-02-02 10:36:21 +0900

    spec内に都度FactoryGirlと記載するのが大変なので省略できるようにconfig修正

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ApiController do
   describe "user" do
     before do
-      @user =  FactoryGirl.build(:user)      
+      @user =  build(:user)
     end
 
     it "shoudl be return user.id" do
